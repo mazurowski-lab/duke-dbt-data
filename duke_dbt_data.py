a32fbad6acb09603569f900217472df98084b780
Mateusz Buda <buda@kth.se>
2020-12-12 14:39:50 +0100

    added functions and examples for bounding boxes

--- duke_dbt_data.py
+++ duke_dbt_data.py
@@ -2,6 +2,7 @@ from typing import AnyStr, BinaryIO, Optional, Union
 
 import matplotlib
 import numpy as np
+import pandas as pd
 import pydicom as dicom
 from skimage.exposure import rescale_intensity
 
@@ -28,6 +29,47 @@ def dcmread_image(
         pixel_array, in_range=(low, high), out_range="dtype"
     )
     return pixel_array
+
+
+def read_boxes(
+    boxes_fp: pd._typing.FilePathOrBuffer, filepaths_fp: pd._typing.FilePathOrBuffer
+) -> pd.DataFrame:
+    """Read pandas DataFrame with bounding boxes joined with file paths"""
+    df_boxes = pd.read_csv(boxes_fp)
+    df_filepaths = pd.read_csv(filepaths_fp)
+    primary_key = ("PatientID", "StudyUID", "View")
+    if not all([key in df_boxes.columns for key in primary_key]):
+        raise AssertionError(
+            f"Not all primary key columns {primary_key} are present in bounding boxes columns {df_boxes.columns}"
+        )
+    if not all([key in df_boxes.columns for key in primary_key]):
+        raise AssertionError(
+            f"Not all primary key columns {primary_key} are present in file paths columns {df_filepaths.columns}"
+        )
+    return pd.merge(df_boxes, df_filepaths, on=primary_key)
+
+
+def draw_box(
+    image: np.ndarray,
+    x: int,
+    y: int,
+    width: int,
+    height: int,
+    color: Optional[Union[int, tuple]] = None,
+    lw=4,
+):
+    """Draw bounding box on the image"""
+    x = min(max(x, 0), image.shape[1] - 1)
+    y = min(max(y, 0), image.shape[0] - 1)
+    if color is None:
+        color = np.max(image)
+    if len(image.shape) > 2 and not hasattr(color, "__len__"):
+        color = (color,) + (0,) * (image.shape[-1] - 1)
+    image[y : y + lw, x : x + width] = color
+    image[y + height - lw : y + height, x : x + width] = color
+    image[y : y + height, x : x + lw] = color
+    image[y : y + height, x + width - lw : x + width] = color
+    return image
 
 
 def _get_dicom_laterality(ds: dicom.dataset.FileDataset) -> str:

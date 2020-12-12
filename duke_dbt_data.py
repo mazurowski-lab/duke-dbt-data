a3f4523a2f3134f964320775c6f5db8b37d6eb11
Mateusz Buda <buda@kth.se>
2020-12-12 14:25:45 +0100

    moved functions from notebook to python file

---
+++ duke_dbt_data.py
@@ -0,0 +1,49 @@
+from typing import AnyStr, BinaryIO, Optional, Union
+
+import matplotlib
+import numpy as np
+import pydicom as dicom
+from skimage.exposure import rescale_intensity
+
+
+def dcmread_image(
+    fp: Union[str, "os.PathLike[AnyStr]", BinaryIO],
+    view: str,
+    index: Optional[np.uint] = None,
+) -> np.ndarray:
+    """Read pixel array from DBT DICOM file"""
+    ds = dicom.dcmread(fp)
+    pixel_array = ds.pixel_array
+    view_laterality = view[0].upper()
+    image_laterality = _get_image_laterality(pixel_array[index or 0])
+    if index is not None:
+        pixel_array = pixel_array[index]
+    if not image_laterality == view_laterality:
+        pixel_array = np.flip(pixel_array, axis=(-1, -2))
+    window_center = _get_window_center(ds)
+    window_width = _get_window_width(ds)
+    low = (2 * window_center - window_width) / 2
+    high = (2 * window_center + window_width) / 2
+    pixel_array = rescale_intensity(
+        pixel_array, in_range=(low, high), out_range="dtype"
+    )
+    return pixel_array
+
+
+def _get_dicom_laterality(ds: dicom.dataset.FileDataset) -> str:
+    """Unreliable - DICOM laterality is incorrect for some cases"""
+    return ds[0x5200, 0x9229][0][0x0020, 0x9071][0][0x0020, 0x9072].value
+
+
+def _get_image_laterality(pixel_array: np.ndarray) -> str:
+    left_edge = np.sum(pixel_array[:, 0])  # sum of left edge pixels
+    right_edge = np.sum(pixel_array[:, -1])  # sum of right edge pixels
+    return "R" if left_edge < right_edge else "L"
+
+
+def _get_window_center(ds: dicom.dataset.FileDataset) -> np.float32:
+    return np.float32(ds[0x5200, 0x9229][0][0x0028, 0x9132][0][0x0028, 0x1050].value)
+
+
+def _get_window_width(ds: dicom.dataset.FileDataset) -> np.float32:
+    return np.float32(ds[0x5200, 0x9229][0][0x0028, 0x9132][0][0x0028, 0x1051].value)
